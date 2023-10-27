3c0804bdd52f4c6b35ce7775757be976b95943f2
Beatriz Vago <beatriz@C02Z88A1LVCF.Home>
2023-10-27 10:55:32 +0100

    req 1

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -1,8 +1,12 @@
+def counter(count_slashes):
+    return count_slashes.count("/")
+
+
 def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
+        deepest_file = max(context["all_files"], key=counter)
         print(f"Deepest file: {deepest_file}")
 
 

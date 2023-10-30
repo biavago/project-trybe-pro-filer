f0484fda83a396a80ca911a0a27906d0acdeae8e
Beatriz Vago <beatriz@C02Z88A1LVCF.Home>
2023-10-30 19:47:14 +0000

    req 5

--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -1,1 +1,40 @@
 from pro_filer.actions.main_actions import show_disk_usage  # NOQA
+import unittest.mock
+
+
+def test_with_empty_file(capsys):
+    show_disk_usage({"all_files": []})
+    expected  = "Total size: 0\n"
+    captured = capsys.readouterr()
+    assert expected == captured.out
+
+
+def test_with_all_files(capsys, tmp_path):
+    path_1 = tmp_path / "tmp_path_1.txt"
+    path_1.write_text("xxxx")
+
+    path_2 = tmp_path / "tmp_path_2.txt"
+    path_2.write_text("xxxxxxxx")
+
+    mock_file = unittest.mock.Mock(return_value="/xxxxxx/")
+
+    context = {
+        "all_files": [
+            f"{path_1}",
+            f"{path_2}"
+        ]
+    }
+
+    expected = (
+        "'/xxxxxx/':                                             "
+        "               8 (66%)\n'/xxxxxx/':                      "
+        "                                      4 (33%)\nTotal size: 12\n"
+    )
+
+    with unittest.mock.patch(
+        "pro_filer.actions.main_actions._get_printable_file_path",
+        mock_file,
+    ):
+        show_disk_usage(context)
+        captured = capsys.readouterr()
+        assert expected == captured.out
